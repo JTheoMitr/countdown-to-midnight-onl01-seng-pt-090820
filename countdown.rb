47baba84be76c11929516f0b11f5ed9da91742cf
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-09-14 03:39:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,4 @@
-def countdown (number)
+def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
@@ -6,7 +6,7 @@ def countdown (number)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown (number)
+def countdown_with_sleep(number)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
